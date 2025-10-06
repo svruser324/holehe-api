55b0c6bc2100e6de0b625301b696e4d09f17ceb0
svruser324 <156933657+svruser324@users.noreply.github.com>
2025-10-06 20:37:05 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 import subprocess
-import re
 
 app = Flask(__name__)
 
@@ -15,7 +14,7 @@ def holehe_lookup():
         return jsonify({"error": "Please provide an email parameter like ?email=someone@example.com"}), 400
 
     try:
-        
+        # Run Holhe
         result = subprocess.run(
             ["holehe", email, "--no-color"],
             capture_output=True,
@@ -27,32 +26,7 @@ def holehe_lookup():
         if not output:
             output = result.stderr.strip() or "No output"
 
-        
-        lines = output.splitlines()
-        registered_sites = []
-
-        for line in lines:
-            line = line.strip()
-            if line.startswith("[+]"):
-                # for site name
-                match = re.search(r"\[\+\]\s*(.+)", line)
-                if match:
-                    site = match.group(1).strip()
-                    
-                    site = re.sub(r"\x1b\[[0-9;]*m", "", site)
-                    registered_sites.append(site)
-
-        
-        registered_sites = [
-            site for site in registered_sites
-            if not any(x in site.lower() for x in ["twitter", "github", "btc"])
-        ]
-
-        return jsonify({
-            "email": email,
-            "registered_on": registered_sites,
-            "count": len(registered_sites)
-        })
+        return jsonify({"output": output})
 
     except subprocess.TimeoutExpired:
         return jsonify({"error": "Holehe scan timed out"}), 504
